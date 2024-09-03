d8015996bb7f61810d7dd53fd03ea24277ab94ba
MattyD04 <matthewdarlow2004@gmail.com>
2024-09-03 10:58:17 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using ST10263027_PROG6212_POE.Models;
+using System.Diagnostics;
+
+namespace ST10263027_PROG6212_POE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TrackClaims()
+        {
+            return View();
+        }
+
+        public IActionResult VerifyClaims()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

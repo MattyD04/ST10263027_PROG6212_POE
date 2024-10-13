fea6e9b13d4ccf4fd14e00cf1ae233747c7591d8
MattyD04 <matthewdarlow2004@gmail.com>
2024-10-13 09:52:23 +0200

    created a ClaimsController that will handle all actions related to the claim

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10263027_PROG6212_POE.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

4dba2ab7bce5b9e9f49a94c7bb83da104550ee94
MattyD04 <matthewdarlow2004@gmail.com>
2024-11-06 13:56:39 +0200

    created the WebApiController file

---
+++ Controllers/WebApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ST10263027_PROG6212_POE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebApiController : ControllerBase
+    {
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,7 +10,6 @@ namespace ST10263027_PROG6212_POE.Data
     public class AppDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
        
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } //parameterless constructor for the AppDbContext class
         public DbSet<Lecturer> Lecturers { get; set; } // This represents the database table for Lecturer entities which links to the Lecturer
         public DbSet<ProgrammeCoordinator> ProgrammeCoordinators { get; set; } // This represents the database table for Programme Coordinators entities which links to ProgrammeCoordinators model
         public DbSet<AcademicManager> AcademicManagers { get; set; } // This represents the database table for Academic Managers entities which links to AcademicManager model
